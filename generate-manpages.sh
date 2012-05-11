37edd236bd993d14d1bdefdda387b01acae12b0a
Scott Moser <smoser@brickies.net>
2012-05-11 18:44:50 -0400

    patch generate-manpages.sh to respect MANDIR
    This makes generate-manpages.sh respect MANDIR if set.

--- generate-manpages.sh
+++ generate-manpages.sh
@@ -1,15 +1,17 @@
 #!/bin/sh -e
 
-mkdir -p man
-rm -rf man/*
+MANDIR=${MANDIR:-./man}
+
+rm -rf "$MANDIR/"*
+mkdir -p "$MANDIR"
 export PYTHONPATH=".:$PYTHONPATH"
 
 # eucacommand-based
 for exe in bin/euca-*; do
-    help2man "$exe" -N -o "man/$(basename $exe).1" -n "$($exe --help | sed '/^$/,$d')"
+    help2man "$exe" -N -o "$MANDIR/${exe##*/}.1" -n "$($exe --help | sed '/^$/,$d')"
 done
 
 # roboto-based
 for exe in bin/euare-* bin/eustore-*; do
-    help2man "$exe" -N -o "man/$(basename $exe).1" -n "$($exe --help | sed '1,2d;/^$/,$d')"
+    help2man "$exe" -N -o "$MANDIR/${exe##*/}.1" -n "$($exe --help | sed '1,2d;/^$/,$d')"
 done
